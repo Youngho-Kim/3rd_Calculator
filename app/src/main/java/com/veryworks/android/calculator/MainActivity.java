aaae35e03e1de676412b20cf10b8132a44687274
javafa <nigne2016@gmail.com>
2017-05-22 12:33:09 +0900

    init

--- app/src/main/java/com/veryworks/android/calculator/MainActivity.java
+++ app/src/main/java/com/veryworks/android/calculator/MainActivity.java
@@ -13,17 +13,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btn1).setOnClickListener(this);
-        findViewById(R.id.btn2).setOnClickListener(this);
-        findViewById(R.id.btn3).setOnClickListener(this);
-        findViewById(R.id.btn4).setOnClickListener(this);
-        findViewById(R.id.btn5).setOnClickListener(this);
-        findViewById(R.id.btn6).setOnClickListener(this);
-        findViewById(R.id.btn7).setOnClickListener(this);
-        findViewById(R.id.btn8).setOnClickListener(this);
-        findViewById(R.id.btn9).setOnClickListener(this);
-        findViewById(R.id.btn0).setOnClickListener(this);
 
+        for(int i=0; i < 10 ; i ++) {
+            // 문자열로 id 가져오기
+            int id = getResources().getIdentifier("btn" + i, "id", getPackageName());
+            findViewById(id).setOnClickListener(this);
+        }
+//        findViewById(R.id.btn1).setOnClickListener(this);
+//        findViewById(R.id.btn2).setOnClickListener(this);
+//        findViewById(R.id.btn3).setOnClickListener(this);
+//        findViewById(R.id.btn4).setOnClickListener(this);
+//        findViewById(R.id.btn5).setOnClickListener(this);
+//        findViewById(R.id.btn6).setOnClickListener(this);
+//        findViewById(R.id.btn7).setOnClickListener(this);
+//        findViewById(R.id.btn8).setOnClickListener(this);
+//        findViewById(R.id.btn9).setOnClickListener(this);
+//        findViewById(R.id.btn0).setOnClickListener(this);
         findViewById(R.id.btnPlus).setOnClickListener(this);
         findViewById(R.id.btnMinus).setOnClickListener(this);
         findViewById(R.id.btnMultiply).setOnClickListener(this);
@@ -41,38 +46,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn1: setPreview(1); break;
             case R.id.btn2: setPreview(2); break;
             case R.id.btn3: setPreview(3); break;
-            case R.id.btn4:
-                break;
-            case R.id.btn5:
-                break;
-            case R.id.btn6:
-                break;
-            case R.id.btn7:
-                break;
-            case R.id.btn8:
-                break;
-            case R.id.btn9:
-                break;
-            case R.id.btn0:
-                break;
+            case R.id.btn4: setPreview(4); break;
+            case R.id.btn5: setPreview(5); break;
+            case R.id.btn6: setPreview(6); break;
+            case R.id.btn7: setPreview(7); break;
+            case R.id.btn8: setPreview(8); break;
+            case R.id.btn9: setPreview(9); break;
+            case R.id.btn0: setPreview(0); break;
             case R.id.btnPlus: setPreview("+"); break;
-            case R.id.btnMinus:
-                break;
-            case R.id.btnMultiply:
-                break;
-            case R.id.btnDivide:
-                break;
-            case R.id.btnResult:
-                result();
-                break;
-            case R.id.btnClear:
-                clear();
-                break;
+            case R.id.btnMinus: setPreview("-"); break;
+            case R.id.btnMultiply: setPreview("*"); break;
+            case R.id.btnDivide: setPreview("/"); break;
+            case R.id.btnResult: result(); break;
+            case R.id.btnClear: clear(); break;
         }
     }
 
     private void result(){
-        // TODO 완성하시오~~~~
+        String current = txtPreview.getText().toString();
+        txtResult.setText(calculate(current));
+    }
+    private String calculate(String preview){
+        String result = preview;
+
+        // TODO 문자열을 쪼갠후 우선순위에 따라 연산하시오
+
+        return result;
     }
 
     private void setPreview(int number){
